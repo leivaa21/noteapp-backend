5ca50718b884ae233cf3dcb4df82fd8a11e676e2
leivaa <adrianleiva212@gmail.com>
2022-10-15 17:29:34 +0200

    feat: createNote and getNoteByID done

--- src/notes/notes.controller.ts
+++ src/notes/notes.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, Get, Request, Response } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Request,
+  Response,
+} from '@nestjs/common';
 import { Request as request, Response as response } from 'express';
 import { NotesService } from './notes.service';
 
@@ -6,10 +13,32 @@ import { NotesService } from './notes.service';
 class NotesController {
   constructor(private readonly service: NotesService) {}
 
+  @Post('/')
+  createNewNote(@Request() req: request, @Response() res: response) {
+    const { title, content } = req.body;
+    if (title === undefined || content === undefined) {
+      throw new Error('Missing Parameters');
+    }
+    this.service.createNote(title, content);
+    res.status(201).json({ message: 'ok!' });
+  }
   @Get('/')
   getNotes(@Request() req: request, @Response() res: response) {
     const notes = this.service.getNotes();
     res.status(200).json({ notes: notes.map((note) => note.json) });
+  }
+  @Get('/:id')
+  getOneNote(
+    @Param('id') id: string,
+    @Request() req: request,
+    @Response() res: response,
+  ) {
+    const note = this.service.getNoteByID(id);
+    if (note === undefined) {
+      res.status(404).json({ message: 'Note not found' });
+      return;
+    }
+    res.status(200).json({ note: note.json });
   }
 }
 
--- src/notes/notes.service.ts
+++ src/notes/notes.service.ts
@@ -1,10 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import Note from './note.entity';
 
+import { randomUUID } from 'crypto';
+
 @Injectable()
 class NotesService {
+  private readonly notes = new Array<Note>();
+  createNote(title: string, content: string): void {
+    const id = randomUUID();
+    const note = new Note(id, title, content);
+    this.notes.push(note);
+  }
   getNotes(): Note[] {
-    return [];
+    return this.notes;
+  }
+  getNoteByID(id: string): Note | undefined {
+    const index = this.notes.findIndex((note) => note.id === id);
+    if (index === -1) {
+      return undefined;
+    }
+    return this.notes[index];
   }
 }
 
